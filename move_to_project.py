151f686e94449f0877b22638600c9a367c25c5ea
TinasheMTapera <teetaps@yahoo.com>
2019-05-29 15:32:23 -0400

    implement moving multi session run to project

--- move_to_project.py
+++ move_to_project.py
@@ -1,10 +1,8 @@
 import json
 import flywheel
+import os
+import logging
 
-# logging stuff
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('fw-heudiconv-gear')
-logger.info("=======: fw-heudiconv starting up :=======")
 
 # start up inputs
 invocation = json.loads(open('config.json').read())
@@ -25,3 +23,35 @@ action = "Export" #config['action']
 use_all_sessions = config['use_all_sessions']
 
 if use_all_sessions:
+
+    # logging stuff
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('fw-heudiconv-gear')
+    logger.info("=======: fw-heudiconv starting up :=======")
+    logger.info("Copying multi-session fMRIprep run to the project level...")
+
+    inputs_list = []
+    try:
+        for key, val in inputs.items():
+
+            if key != "api_key":
+                fname = val['location']['name']
+                obj = fw.get(val['hierarchy']['id'])
+                input_ref = obj.get_file(fname).ref()
+                inputs_list.append(input_ref)
+
+        myanalysis = project_container.add_analysis(label = analysis_container.label, inputs = inputs_list)
+        myanalysis.add_note(
+            (
+            "Copied from a multi-session fMRIprep analysis gear.\n",
+            " ID: {}".format(analysis_container.id)
+            )
+        )
+        outputs = os.listdir("/flywheel/v0/output/")
+        for x in outputs:
+            myanalysis.upload_output("/flywheel/v0/output/"+x)
+    except Exception as e:
+        logger.warning("Failed to copy multi-session analysis to the project level!")
+        logger.warning("Your gear possibly still ran successfully, please check the origin for the analysis. ID: {}".format(analysis_container.id))
+        logger.warning(e)
+    logger.info("Done!")
